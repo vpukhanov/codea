d5e76c0de755e70ba5b4b7db3de1658e5b56c22a
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-11-29 21:29:36 +0300

    Bind document content to a view model

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -8,20 +8,8 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Codea
 {
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    public sealed partial class MainPage : Page
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public string CursorPositionHint
-        {
-            get { return _cursorRow + ":" + _cursorColumn; }
-        }
-
-        private uint _cursorRow = 1;
-        private uint _cursorColumn = 1;
-
-        private readonly DispatcherTimer _slowUpdateTimer;
-
         public MainPage()
         {
             InitializeComponent();
@@ -30,10 +18,6 @@ namespace Codea
             coreTitleBar.LayoutMetricsChanged += MainPage_LayoutMetricsChanged;
 
             Window.Current.SetTitleBar(AppTitleBar);
-
-            _slowUpdateTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
-            _slowUpdateTimer.Tick += SlowUpdateTimer_Tick;
-            _slowUpdateTimer.Start();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -41,20 +25,6 @@ namespace Codea
             base.OnNavigatedTo(e);
 
             CodeEditor.Focus(FocusState.Programmatic);
-        }
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private async void SlowUpdateTimer_Tick(object sender, object e)
-        {
-            var position = await CodeEditor.GetPositionAsync();
-            _cursorRow = position.LineNumber;
-            _cursorColumn = position.Column;
-
-            OnPropertyChanged("CursorPositionHint");
         }
 
         private void MainPage_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
---
+++ View Models/DocumentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codea.View_Models
+{
+    public class DocumentViewModel : INotifyPropertyChanged
+    {
+        private string content;
+        public string Content
+        {
+            get => content;
+            set
+            {
+                content = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
